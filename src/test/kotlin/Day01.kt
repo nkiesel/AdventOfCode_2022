be17fbbf387c9e779621b9da61155e462cc3935e
Norbert Kiesel <nk@iname.com>
2022-11-30 21:33:36 -0800

    Day01

---
+++ src/test/kotlin/Day01.kt
@@ -0,0 +1,44 @@
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+
+class Day01 {
+    private val sample = """
+        1000
+        2000
+        3000
+
+        4000
+
+        5000
+        6000
+
+        7000
+        8000
+        9000
+
+        10000
+    """.trimIndent().lines()
+
+    @Test
+    fun testOne(input: List<String>) {
+        one(sample) shouldBe 24000
+        one(input) shouldBe 70764
+    }
+
+    @Test
+    fun testTwo(input: List<String>) {
+        two(sample) shouldBe 45000
+        two(input) shouldBe 203905
+    }
+
+    private fun one(input: List<String>): Int = input.chunkedBy { it.isEmpty() }.maxOf { it.map(String::toInt).sum() }
+
+    private fun two(input: List<String>): Int = input.chunkedBy { it.isEmpty() }.map { it.map(String::toInt).sum() }.sorted().takeLast(3).sum()
+}
+
+/*
+As usual, AoC starts very simple. I still had to remember that sorted sorts naturally and I thus had to use `takeLast` and not `take`.
+
+Also, as usual we could now replace `one` with `two` by adding a `, n: Int = 1` parameter and use that in `takeLast`
+ */
