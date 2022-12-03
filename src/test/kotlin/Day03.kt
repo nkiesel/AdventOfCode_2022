b86c716b2d2af633b7f1218453975de73d90d095
Norbert Kiesel <nk@iname.com>
2022-12-02 23:01:34 -0800

    some more changes for Day03

--- src/test/kotlin/Day03.kt
+++ src/test/kotlin/Day03.kt
@@ -25,19 +25,23 @@ class Day03 {
     }
 
     private fun one(input: List<String>): Int = input
-        .map { it.chunked(it.length / 2) }
-        .map { (l, r) -> (l.toSet() intersect r.toSet()).first() }
-        .sumOf { it.code + if (it in 'a'..'z') 1 - 'a'.code else 27 - 'A'.code }
+        .map { line -> line.chunked(line.length / 2) { it.toSet() } }
+        .map { (l, r) -> (l intersect r).single() }
+        .sumOf { it - if (it.isLowerCase()) ('a' - 1) else ('A' - 27) }
 
     private fun two(input: List<String>): Int = input
         .chunked(3) { g -> g.map { it.toSet() } }
-        .map { g -> g.reduce { acc, l -> acc intersect l }.first() }
-        .sumOf { it.code + if (it in 'a'..'z') 1 - 'a'.code else 27 - 'A'.code }
+        .map { g -> g.reduce { acc, l -> acc intersect l }.single() }
+        .sumOf { it - if (it.isLowerCase()) ('a' - 1) else ('A' - 27) }
 }
 
 /*
-Much simpler than day 2 because of the easy way in Kotlin to convert strings to sets of chars and intersect sets.
+Much simpler than day 2 because of the easy way to convert strings to sets of chars and intersect sets.
 
 Initially used `g[0] intersect g[1] intersect g[2]`, but then switched to `reduce` because that allows handling
 different group sizes w/o code change of that part.
+
+Also, I first forgot that `chunked` has an optional transformation function and thus initially used `chunked` + `map`.
+Final optimization was realizing that `'b' - 'a'` is the same as `'b'.code - 'a'.code` and that I thus could
+remove the `.code`.
 */
