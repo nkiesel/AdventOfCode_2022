9d89d7965c103e4be50f9f7b10eb81bb060fcd6c
Norbert Kiesel <nk@iname.com>
2022-12-23 12:43:40 -0800

    Day 23 minor improvements

--- src/test/kotlin/Day23.kt
+++ src/test/kotlin/Day23.kt
@@ -24,21 +24,28 @@ class Day23 {
         two(input) shouldBe 950
     }
 
-    data class Elf(var x: Int, var y: Int) {
-        var px = x
-        var py = y
+    data class Elf(val x: Int, val y: Int) {
+        var px = 0
+        var py = 0
 
-        fun neighbors(elves: Set<Elf>): Map<Char, Set<Elf>> {
+        fun reset() {
+            px = x
+            py = y
+        }
+
+        fun neighbors(elves: Set<Elf>): Map<Char, Boolean> {
             val n8 = listOf(-1 to -1, -1 to 0, -1 to 1, 0 to -1, 0 to 1, 1 to -1, 1 to 0, 1 to 1)
                 .map { (dx, dy) -> Elf(x + dx, y + dy) }
                 .filter { it in elves }
-            if (n8.isEmpty()) return emptyMap()
-            return mapOf(
-                'n' to n8.filter { it.y == y - 1 }.toSet(),
-                's' to n8.filter { it.y == y + 1 }.toSet(),
-                'e' to n8.filter { it.x == x + 1 }.toSet(),
-                'w' to n8.filter { it.x == x - 1 }.toSet(),
-            )
+            return with(n8) {
+                if (isEmpty()) emptyMap() else
+                    mapOf(
+                        'n' to any { it.y == y - 1 },
+                        's' to any { it.y == y + 1 },
+                        'e' to any { it.x == x + 1 },
+                        'w' to any { it.x == x - 1 },
+                    )
+            }
         }
     }
 
@@ -52,57 +59,50 @@ class Day23 {
         }
     }
 
-    private fun round(elves: Set<Elf>, di: Int): Boolean {
+    private fun round(elves: Set<Elf>, di: Int): Set<Elf> {
         val dirs = "nswe".toCharArray()
-        elves.forEach { it.px = it.x; it.py = it.y }
+        elves.forEach { it.reset() }
         for (elf in elves) {
-            val n8 = elf.neighbors(elves)
-            if (n8.isEmpty()) continue
-            val d = dirs.indices.map { dirs[(it + di).mod(dirs.size)] }.find { n8[it]!!.isEmpty() } ?: continue
+            val n = elf.neighbors(elves)
+            if (n.isEmpty()) continue
+            val d = dirs.indices.map { dirs[(it + di).mod(dirs.size)] }.find { n[it] == false } ?: continue
             when (d) {
                 'n' -> elf.py--
                 's' -> elf.py++
                 'e' -> elf.px++
                 'w' -> elf.px--
-                else -> error("Invalid direction")
             }
         }
-        var stable = true
-        for (elf in elves) {
-            if ((elf.x != elf.px || elf.y != elf.py) && elves.count { it.px == elf.px && it.py == elf.py } == 1) {
-                elf.x = elf.px
-                elf.y = elf.py
-                stable = false
+        return elves.map { elf ->
+            with (elf) {
+                if (x == px && y == py || elves.count { it.px == px && it.py == py } != 1) {
+                    elf
+                } else {
+                    Elf(px, py)
+                }
             }
-        }
-        return stable
+        }.toSet()
     }
 
     private fun one(input: List<String>): Int {
-        var elves = parse(input).toSet()
-        for (i in 0..9) {
-            round(elves, i)
-            // recompute the set based on updated elves
-            elves = elves.toSet()
+        var elves = parse(input)
+        repeat(10) { r ->
+            elves = round(elves, r)
         }
-        val minX = elves.minOf { it.x }
-        val maxX = elves.maxOf { it.x }
-        val minY = elves.minOf { it.y }
-        val maxY = elves.maxOf { it.y }
-        return (maxX - minX + 1) * (maxY - minY + 1) - elves.size
+        return (elves.maxOf { it.x } - elves.minOf { it.x } + 1) * (elves.maxOf { it.y } - elves.minOf { it.y } + 1) - elves.size
     }
 
     private fun two(input: List<String>): Int {
-        var elves = parse(input).toSet()
-        for (i in 0..Int.MAX_VALUE) {
-            if (round(elves, i)) return i + 1
-            // recompute the set based on updated elves
-            elves = elves.toSet()
+        var elves = parse(input)
+        repeat(Int.MAX_VALUE) { r ->
+            val next = round(elves, r)
+            if (next == elves) return r + 1
+            elves = next
         }
         return -1
     }
-
 }
+
 /*
 Again relatively simple, and again I shot myself in the foot multiple times.  First set of
 issues was that I changed px instead of py for n/s.  That was relatively simple to find by
@@ -110,4 +110,8 @@ comparing the sample maps.  The bigger issue was that I use a Set<Elf>, but then
 elves after they were inserted into the set.  This then produces wrong results for "in" tests
 because these are based on the hashCode of the original x/y.  I added a stupid fix for this,
 but this is clearly a lesson learned!
+
+Update: Made elves immutable, and simplified `neighbors` to a Map to Boolean instead of a
+Set<Elf>. I initially anticipated that we would have to handle these differently in part 2;
+classical over-engineering.
  */
